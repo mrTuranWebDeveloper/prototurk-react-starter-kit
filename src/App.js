0d9f0e93adbff23335108251b8236342afaf1064
ömer turan <omerturandeveloper@gmail.com>
2023-10-06 14:36:20 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import logo from "./logo.svg"
-import {useEffect} from "react";
-import Test from "./Test"
-import { Title } from "./Components";
-import Bootstrap from "./Bootstrap";
-import Tailwind from "./Tailwind";
+import './style.scss'
+import { createElement } from "react"
 
-import './tailwind.css'
-import styles from './App.module.css'
+
+
+
 
 function App() {
 
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      // analytici bilgilendir
-    }
-  }, [])
+  const todos = ['todo1', 'todo2', 'todo3']
+  // const h1 = createElement('h1', null, 'mrTuranDeveloper')
+  // const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
+  // return createElement('main', {
+  //   className: 'test',
+  //   id: 'main'
+  // }, h1, ul)
+
+  const searchFunction = () => {
+    alert('HELLO')
+  }
 
   return (
-    <div className={styles.App}>
-      <Title>{process.env.NODE_ENV}</Title>
-      <Title theme="dark">{process.env.NODE_ENV}</Title>
-      <p>
-        {process.env.REACT_APP_API_URL}
-      </p>
-      <Test />
-      {process.env.NODE_ENV === 'production' && (
-        <>
-          <img src="/logo192.png" alt=""/>
-          <img src={logo} alt=""/>
-        </>
-      )}
-      <Bootstrap />
-      <Tailwind />
-    </div>
+   <main id='main' className='test'>
+    <h1 tabIndex="3"style={{color:'red', backgroundColor:'yellow'}}>mrTuranDeveloper</h1>
+    <label htmlFor="search" tabIndex="2" onClick={searchFunction}>ARAMA</label>
+    <input type="text" id='search' tabIndex="1" />
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>
+          {todo}
+        </li>
+      ))}
+    </ul>
+   </main>
   );
 }
 
