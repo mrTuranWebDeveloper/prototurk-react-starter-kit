f549844b264e739bec02349ddc89faf585199a7e
ömer turan <omerturandeveloper@gmail.com>
2023-10-23 21:54:03 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import './tailwind.css'
-import Button from './components/Button'
-import Tab from './components/Tab'
+// import './tailwind.css'
+// import Button from './components/Button'
+// import Tab from './components/Tab'
 import { useState } from 'react'
+import Test from './components/Test'
+
 
 
 
@@ -10,9 +12,18 @@ import { useState } from 'react'
 
 function App() {
 
-  const todos = ['todo1', 'todo2', 'todo3']
+  const [show, setShow] = useState(false)
+  return (
+    <>
+      <button onClick={()=> setShow(show => !show)}>
+        {show ? 'Gizle' : 'Göster'}
+      </button>
+      {show && <Test />}
+    </>
+  )
+  // const todos = ['todo1', 'todo2', 'todo3']
 
-  const [activeTab, setActiveTab] = useState(1)
+  // const [activeTab, setActiveTab] = useState(1)
   // const h1 = createElement('h1', null, 'mrTuranDeveloper')
   // const ul = createElement('ul', null, todos.map(todo => createElement('li', null, todo)))
   // return createElement('main', {
@@ -20,46 +31,46 @@ function App() {
   //   id: 'main'
   // }, h1, ul)
 
-  const searchFunction = () => {
-    alert('HELLO')
-  }
+  // const searchFunction = () => {
+  //   alert('HELLO')
+  // }
 
   
 
-  return (
-   <>
+//   return (
+//    <>
     
-   <div style = {{padding: 20}}>
-    <button onClick={()=> setActiveTab(2)}>
-      Aktif Tabı Değiştir
-    </button>
-    <Tab activeTab={activeTab} setActiveTab={setActiveTab}>
-      <Tab.Panel title="Profil">1. Tab</Tab.Panel>
-      <Tab.Panel title="Hakkında">2. Tab</Tab.Panel>
-      <Tab.Panel title="Ayarlar">3. Tab</Tab.Panel>
-    </Tab>
-   </div>
+//    <div style = {{padding: 20}}>
+//     <button onClick={()=> setActiveTab(2)}>
+//       Aktif Tabı Değiştir
+//     </button>
+//     <Tab activeTab={activeTab} setActiveTab={setActiveTab}>
+//       <Tab.Panel title="Profil">1. Tab</Tab.Panel>
+//       <Tab.Panel title="Hakkında">2. Tab</Tab.Panel>
+//       <Tab.Panel title="Ayarlar">3. Tab</Tab.Panel>
+//     </Tab>
+//    </div>
 
 
-    <div style={{padding: 20}}>
-    <Button text="Buton Örneği"/>
-    <Button text="Buton Örneği" variant="success"/>
-    <Button text="Buton Örneği" variant="danger"/>
-    <Button text="Buton Örneği" variant="warning"/>
-    </div>
-    <h1 tabIndex="3"style={{color:'red', backgroundColor:'yellow'}}>mrTuranDeveloper</h1>
-    <label htmlFor="search" tabIndex="2" onClick={searchFunction}>ARAMA</label>
-    <input type="text" id='search' tabIndex="1" />
-    <ul>
+//     <div style={{padding: 20}}>
+//     <Button text="Buton Örneği"/>
+//     <Button text="Buton Örneği" variant="success"/>
+//     <Button text="Buton Örneği" variant="danger"/>
+//     <Button text="Buton Örneği" variant="warning"/>
+//     </div>
+//     <h1 tabIndex="3"style={{color:'red', backgroundColor:'yellow'}}>mrTuranDeveloper</h1>
+//     <label htmlFor="search" tabIndex="2" onClick={searchFunction}>ARAMA</label>
+//     <input type="text" id='search' tabIndex="1" />
+//     <ul>
       
-      {todos.map((todo, index) => (
-        <li key={index}>
-          {todo}
-        </li>
-      ))}
-    </ul>
-   </>
-  );
+//       {todos.map((todo, index) => (
+//         <li key={index}>
+//           {todo}
+//         </li>
+//       ))}
+//     </ul>
+//    </>
+//   );
 }
 
 export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+export default function Test() {
+
+    const [postId, setPostId] = useState(1)
+    const [post, setPost] = useState(false)
+
+    useEffect(()=> {
+        console.log('component ilk yüklendiğinde çalışır');
+        return ()=> {
+            console.log('component destroyed');
+        }
+    }, [])
+
+    useEffect(()=> {
+        fetch('https://jsonplaceholder.typicode.com/posts/${postId}')
+        .then(data => setPost(data))
+    }, [postId])
+
+    // useEffect(()=> {
+    //     console.log('component render oldu!');
+    // })
+
+    return (
+        <div>
+            <h3>{postId}</h3>
+            {post && JSON.stringify(post)}
+
+            <button onClick={()=> setPostId(postId => postId + 1)}>Sonraki Konu</button>
+            <hr />
+            test component
+        </div>
+    )
+} 
